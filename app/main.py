dfbe4d1382ae865f004e3b6fdbc99fd6a00810b3
Siraj Samsudeen <mailsiraj@gmail.com>
2023-12-24 17:11:54 +0530

    stage 3 - 404

--- app/main.py
+++ app/main.py
@@ -10,8 +10,15 @@ def main():
 
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     connection, address = server_socket.accept()  # wait for client
-    print("connection from", address, connection.recv(1024).decode())
-    connection.send('HTTP/1.1 200 OK\r\n\r\n'.encode())
+    print("connection from", address)
+    client_message = connection.recv(1024).decode()
+    line1 = client_message.splitlines()[0]
+    path = line1.split()[1]
+    if path == '/':
+        response = 'HTTP/1.1 200 OK\r\n\r\n'
+    else:
+        response = 'HTTP/1.1 404 Not Found\r\n\r\n'
+    connection.send(response.encode())
     connection.close()
 
 
