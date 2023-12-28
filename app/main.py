05f6f1a579147e728f0dc79e3381b59582343c27
Siraj Samsudeen <mailsiraj@gmail.com>
2023-12-28 12:12:07 +0530

    POST file content

--- app/main.py
+++ app/main.py
@@ -26,8 +26,10 @@ def main():
 
 def handle_client(connection,args):
     client_message = connection.recv(1024).decode()
-    headers = client_message.splitlines()
-    path = headers[0].split()[1]
+    *headers, content = client_message.splitlines()
+    # print(f"{repr(client_message)=}")
+    # print(f"{headers=}, {content=}")
+    method, path, _ = headers[0].split()
     header_lines = []
     if path == '/':
         header_lines.append('HTTP/1.1 200 OK')
@@ -35,19 +37,29 @@ def handle_client(connection,args):
     elif path.startswith('/files'):
         file_name = path.removeprefix('/files/')
         path = Path(args.directory)
-        file_path = list(path.glob(file_name))
-        print(args.directory, file_name, file_path)
-        if not file_path:
-            header_lines.append('HTTP/1.1 404 Not Found')
+
+        if method == 'POST':
+            path = path / file_name
+            path.write_bytes(content.encode())
+            print("wrote ", path)
+            header_lines.append('HTTP/1.1 201 OK')
             header_lines.append("\r\n")
+
+            
         else:
-            with open(file_path[0]) as file:
-                file_content = file.read()
-            header_lines.append('HTTP/1.1 200 OK')
-            header_lines.append('Content-Type: application/octet-stream')
-            header_lines.append(f'Content-Length: {len(file_content.encode())}')
-            header_lines.append("")
-            header_lines.append(file_content)
+            file_path = list(path.glob(file_name))
+            print(args.directory, file_name, file_path)
+            if not file_path:
+                header_lines.append('HTTP/1.1 404 Not Found')
+                header_lines.append("\r\n")
+            else:
+                with open(file_path[0]) as file:
+                    file_content = file.read()
+                header_lines.append('HTTP/1.1 200 OK')
+                header_lines.append('Content-Type: application/octet-stream')
+                header_lines.append(f'Content-Length: {len(file_content)}')
+                header_lines.append("")
+                header_lines.append(file_content)
     elif path.startswith('/user-agent'):
         header_lines.append('HTTP/1.1 200 OK')
         header_lines.append('Content-Type: text/plain')
